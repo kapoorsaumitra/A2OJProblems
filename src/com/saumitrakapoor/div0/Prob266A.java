33c9c9ed468468e5b4b9b7d7ed19e7770e07a8fa
Saumitra Kapoor <saumitrakapoor24@gmail.com>
2022-09-03 21:22:55 +0530

    added solution for problem 266 A

---
+++ src/com/saumitrakapoor/div0/Prob266A.java
@@ -0,0 +1,72 @@
+package com.saumitrakapoor.div0;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Prob266A {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader s = new FastReader();
+        int n = s.nextInt();
+        String str = s.nextLine();
+        if (n==1)
+            System.out.println(0);
+        else
+        {
+            int c =0;
+            for (int p =0;p<n-1;p++){
+                if (str.charAt(p)==str.charAt(p+1))
+                    c++;
+            }
+            System.out.println(c);
+        }
+
+    }
+
+
+}
