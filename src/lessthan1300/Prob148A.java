96ca984bc2c2209f705ad1ee0c0de291dfa31dab
Saumitra Kapoor <saumitrakapoor24@gmail.com>
2022-09-05 18:50:45 +0530

    added solution for problem 148 A

---
+++ src/lessthan1300/Prob148A.java
@@ -0,0 +1,71 @@
+package lessthan1300;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Prob148A {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader s = new FastReader();
+        int k = s.nextInt();
+        int l = s.nextInt();
+        int m = s.nextInt();
+        int n = s.nextInt();
+        int d = s.nextInt();
+        int c =0;
+        for (int i =1;i<=d;i++){
+            if (i%k ==0 || i%l ==0 || i%m ==0 || i%n ==0)
+                c++;
+        }
+        System.out.println(c);
+
+
+    }
+
+
+}
